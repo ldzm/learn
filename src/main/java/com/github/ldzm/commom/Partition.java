528833233fd5434b0d6d86b95ee4dc3bc4ba2e90
ldzm <ldzm_zg@126.com>
2017-08-26 17:12:28 +0800

    重构Partition

--- src/main/java/com/github/ldzm/commom/Partition.java
+++ src/main/java/com/github/ldzm/commom/Partition.java
@@ -1,36 +1,35 @@
 package com.github.ldzm.commom;
 
 public class Partition {
-    public static  int partion(int[] nums, int start, int end) {
+    public static  int partition(int[] numbs, int start, int end) {
 
         int index = randomRange(start, end);
-        swap(nums, index, end);
+        swap(numbs, index, end);
         int small = start - 1;
 
         for(index = start; index < end; index++) {
-            if (nums[index] <= nums[end]) {
+            if (numbs[index] <= numbs[end]) {
                 small++;
                 if (small != index) {
-                    swap(nums, small, index);
+                    swap(numbs, small, index);
                 }
             }
         }
 
         small++;
-        swap(nums, small, end);
+        swap(numbs, small, end);
 
         return small;
     }
 
     private static  int randomRange(int start, int end) {
 
-        int result = start + (int) ((end - start + 1) * Math.random());
-        return result;
+        return start + (int) ((end - start + 1) * Math.random());
     }
 
-    private static  void swap(int [] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    private static  void swap(int [] numbs, int i, int j) {
+        int temp = numbs[i];
+        numbs[i] = numbs[j];
+        numbs[j] = temp;
     }
 }
