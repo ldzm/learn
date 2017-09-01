6141955daf060fe626967bf6131b35e9267c6978
ldzm <ldzm_zg@126.com>
2017-09-01 13:04:42 +0800

    修改链表相加算法实现

--- src/main/java/com/github/ldzm/lintcode/AddLists.java
+++ src/main/java/com/github/ldzm/lintcode/AddLists.java
@@ -4,7 +4,7 @@ public class AddLists {
     /**
      * @param l1: the first list
      * @param l2: the second list
-     * @return: the sum list of l1 and l2
+     * @return the sum list of l1 and l2
      */
     public ListNode addLists(ListNode l1, ListNode l2) {
 
@@ -28,25 +28,30 @@ public class AddLists {
                 head = current = l1;
             } else {
                 current.next = l1;
+                current = current.next;
             }
 
             l1 = l1.next;
             l2 = l2.next;
         }
 
-        if(l1 == null) {
-            if(l2 != null) {
-                l2.val = temp / 10;
-            }
-
-            current.next = l2;
+        if (l1 == null && l2 == null && (temp / 10 == 1)) {
+            current.next = new ListNode(1);
+            current = current.next;
         }
-        if(l2 == null) {
-            if(l1 != null) {
-                l1.val = temp / 10;
-            }
 
+        if (l1 == null && l2 != null) {
+
+            l2.val = l2.val + temp / 10;
+            current.next = l2;
+            current = current.next;
+        }
+
+        if (l2 == null && l1 != null) {
+
+            l1.val = l1.val + temp / 10;
             current.next = l1;
+            current = current.next;
         }
 
         return head;
@@ -54,22 +59,20 @@ public class AddLists {
 
     public static void main(String[] args) {
 
-        ListNode head = null;
-        ListNode current = head = new ListNode(3);;
+        ListNode head;
+        ListNode current = head = new ListNode(3);
+        current.next = new ListNode(1);
         current = current.next;
-        current = new ListNode(1);
-        current = current.next;
-        current = new ListNode(5);
+        current.next = new ListNode(5);
 
-        ListNode head2 = null;
+        ListNode head2;
         current = head2 = new ListNode(5);
+        current.next = new ListNode(9);
         current = current.next;
-        current = new ListNode(9);
-        current = current.next;
-        current = new ListNode(2);
+        current.next = new ListNode(4);
 
-        ListNode result = new AddLists().addLists(head, head2);
-        current  = result;
+        current = new AddLists().addLists(head, head2);
+
         while(current != null) {
             System.out.println(current.val);
             current = current.next;
